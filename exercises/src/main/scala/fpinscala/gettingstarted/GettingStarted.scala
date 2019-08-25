bec18fd3ceac877885d63710b7e05c413e542c0f
Colin Strong <colin@colinstrong.ca>
2019-08-25 11:22:41 -0700

    Completed Chapter 2

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -36,7 +36,14 @@ object MyModule {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(i: Int, prev: Int, curr: Int): Int = {
+      if (i == 0) prev
+      else go(i - 1, curr, prev + curr)
+    }
+    go(n, 0, 1)
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
@@ -140,26 +147,36 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(i: Int): Boolean = {
+      if (i >= as.length) true
+      else if (! gt(as(i - 1), as(i))) false
+      else go(i + 1)
+    }
+    if (as.length == 0 || as.length == 1) true
+    else go(1)
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
 
-  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
-    (b: B) => f(a, b)
+  def partial1[A,B,C](a: A, f: (A,B) => C): B => C = {
+    b => f(a, b)
+  }
 
   // Exercise 3: Implement `curry`.
 
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+  def curry[A,B,C](f: (A, B) => C): A => B => C =
+    a => b => f(a, b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a, b) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -174,5 +191,31 @@ object PolymorphicFunctions {
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    a => f(g(a))
+}
+
+object TestPolymorphicFunctions {
+  import PolymorphicFunctions._
+
+  def main(args: Array[String]): Unit = {
+    testIsSorted()
+  }
+
+  def testIsSorted(): Unit = {
+    val empty: Array[Int] = Array()
+    val one = Array(3)
+    val unsorted = Array(3,1,2)
+    val sorted = Array(1,2,3)
+
+    val testFn = (arr: Array[Int]) => isSorted(arr, (x: Int, y: Int) => x < y)
+
+    testArray(empty, true, testFn(empty))
+    testArray(one, true, testFn(one))
+    testArray(unsorted, false, testFn(unsorted))
+    testArray(sorted, true, testFn(sorted))
+  }
+
+  def testArray[A, B](arr: Array[A], expected: B, result: B): Unit = {
+    println(s"Testing ${arr.mkString(",")}. Expected: ${expected.toString}. Result: ${result.toString}")
+  }
 }
